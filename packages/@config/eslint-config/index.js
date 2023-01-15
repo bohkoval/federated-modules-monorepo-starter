42dfdf63924128e7ebac7b4372020c14e999190c
Bohdan Koval <bohdankoval@Bohdans-MacBook-Pro.local>
2023-01-15 15:14:23 +0100

    configuring tsconfig and eslint

---
+++ packages/@config/eslint-config/index.js
@@ -0,0 +1,31 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "turbo",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "jsx-a11y"],
+  rules: {
+    "linebreak-style": ["error", "unix"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
