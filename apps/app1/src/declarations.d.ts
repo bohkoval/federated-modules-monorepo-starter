6713d5cbefa5f253f821e0364fbf20728cc7e5cf
Bohdan Koval <bohkoval@gmail.com>
2023-01-20 18:43:59 +0100

    project cleanup. remove storybook, since it to be set up personalized per project needs (if needed). No need to include it in generic starter and easier to add and setup properly as need (as standalone app)

--- apps/app1/src/declarations.d.ts
+++ apps/app1/src/declarations.d.ts
@@ -2,7 +2,3 @@
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.png';
-// to be able to import jpg/jpeg/png files
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.png';
